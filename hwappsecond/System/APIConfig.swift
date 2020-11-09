5c91c54d948fcf3e10f7acad169285bae950e632
Alex Kh <alexander@udev.dev>
2020-11-09 04:08:58 +0400

    Added misssing parts

--- hwappsecond/System/APIConfig.swift
+++ hwappsecond/System/APIConfig.swift
@@ -5,11 +5,11 @@
 
 import Foundation
 
-open class DummyAPIConfig {
+open class APIConfig: DummyApiConfig {
     private static var dummyApiKey = "5f7c2dcc582b4b0e578d238d"
     public static var basePath = "https://dummyapi.io/data/api"
     public static var credential: URLCredential?
-    public static var customHeaders: [String:String] = ["app-id": DummyAPIConfig.dummyApiKey]
+    public static var customHeaders: [String:String] = ["app-id": APIConfig.dummyApiKey]
     public static var requestBuilderFactory: RequestBuilderFactory = URLSessionRequestBuilderFactory()
     public static var apiResponseQueue: DispatchQueue = .main
 }
